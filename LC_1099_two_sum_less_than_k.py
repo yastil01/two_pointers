fa54d08ff5bb2b26eb80200cbb452ea2b9d6d9d3
yastil01 <63788022+yastil01@users.noreply.github.com>
2020-09-27 14:39:13 -0700

    Create LC_1099_two_sum_less_than_k.py

---
+++ LC_1099_two_sum_less_than_k.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSumLessThanK(self, nums: List[int], k: int) -> int:
+        if not nums:
+            return -1
+        
+        nums.sort() 
+        maxSum = nums[0]
+        left , right = 0, len(nums)-1
+        while left < right:
+            if nums[left] + nums[right] < k:
+                maxSum = max(maxSum, nums[left] + nums[right])
+                left += 1
+            else:
+                right -= 1
+        
+        if maxSum == nums[0]:
+            return -1
+        return maxSum
