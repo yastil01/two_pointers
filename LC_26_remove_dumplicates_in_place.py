7faa90bd14c8ba234fb53f8becc93be282ff1506
yastil01 <63788022+yastil01@users.noreply.github.com>
2020-09-27 00:17:46 -0700

    Update LC_26_remove_dumplicates_in_place.py

--- LC_26_remove_dumplicates_in_place.py
+++ LC_26_remove_dumplicates_in_place.py
@@ -2,7 +2,7 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         slow = 1
         for fast in range(1, len(nums)):
-            if nums[fast] != nums[fast - 1]:
+            if nums[slow-1] != nums[fast]:
                 nums[slow] = nums[fast]
                 slow += 1
         
