b63945e5491e9af7546a2569c1b04e290a133c42
yastil01 <63788022+yastil01@users.noreply.github.com>
2021-01-05 22:50:45 -0800

    Create LC_443_string_compression.py

---
+++ LC_443_string_compression.py
@@ -0,0 +1,18 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        if not chars:
+            return 0
+        
+        start = index = 0
+        for end in range(len(chars)+1):
+            if end == len(chars) or chars[end] != chars[start]:
+                chars[index] = chars[start]
+                index += 1
+                freq = end -start
+                if freq > 1:
+                    for c in str(freq):
+                        chars[index] = c
+                        index += 1
+                start = end
+        
+        return index
